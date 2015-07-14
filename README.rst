4ec6bab9fd3d2fc1a1c6636735b6dcc3195c2280
Dimas Abreu Dutra <dimasad@ufmg.br>
2015-07-14 17:55:30 -0300

    Renamed package to avoid conflict with other packages on PyPI.

---
+++ README.rst
@@ -0,0 +1,5 @@
+yaipopt
+=======
+
+Yet Another python package with bindinds for the IPOPT nonlinear optimization
+solver of COIN-OR.
--- setup.py
+++ setup.py
@@ -6,12 +6,20 @@ from setuptools.extension import Extension
 from Cython.Distutils import build_ext
 
 
-def flag_dict(flags):
-    d = {}
-    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    for token in flags.split():
-        d.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-    return d
+DESCRIPTION = open("README.rst", encoding="utf-8").read()
+
+CLASSIFIERS = '''\
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved
+Operating System :: MacOS
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Scientific/Engineering
+Topic :: Software Development'''
 
 
 if 'IPOPT_CFLAGS' in os.environ:
@@ -29,17 +37,34 @@ else:
         flags = ''
 
 
-ipopt_cfg = flag_dict(flags)
-wrapper_ext = Extension("ipopt.wrapper", ["ipopt/wrapper.pyx"], **ipopt_cfg)
+def flag_dict(flags):
+    d = {}
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in flags.split():
+        d.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return d
 
-setup(name="ipopt",
-      version='0.1',
-      description='Python bindings for IPOPT nonlinear optimization solver.',
-      author='Dimas Abreu Dutra',
-      author_email='dimasadutra@gmail.com',
-      url='http://github.com/dimasad/python-ipopt',
-      test_suite='nose.collector',
-      tests_require=['nose>=1.0'],
-      install_requires='distribute',
-      packages=find_packages(),
-      cmdclass={"build_ext": build_ext}, ext_modules=[wrapper_ext])
+
+ipopt_cfg = flag_dict(flags)
+wrapper_ext = Extension("yaipopt.wrapper", ["yaipopt/wrapper.pyx"], **ipopt_cfg)
+
+
+setup(
+    name="yaipopt",
+    version='0.1.dev1',
+    test_suite='nose.collector',
+    tests_require=['nose>=1.0'],
+    install_requires=['cython', 'distribute', 'numpy'],
+    packages=find_packages(),
+    cmdclass={"build_ext": build_ext}, ext_modules=[wrapper_ext],
+    
+    # metadata for upload to PyPI
+    author='Dimas Abreu Dutra',
+    author_email='dimasad@ufmg.br',
+    description='Python bindings for IPOPT nonlinear optimization solver.',
+    long_description=DESCRIPTION,
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    classifiers=CLASSIFIERS.split('\n'),
+    license="MIT",
+    url='http://github.com/dimasad/cyipopt',
+)
