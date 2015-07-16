bf9813264357bafc855d14ecdf9dbbcd10f0cb66
Dimas Abreu Dutra <dimasadutra@gmail.com>
2015-07-16 19:05:36 -0300

    setup: safe import of cython in build script.

--- setup.py
+++ setup.py
@@ -45,14 +45,21 @@ def ipopt_opts():
 
 wrapper = Extension("yaipopt.wrapper", ["yaipopt/wrapper.pyx"], **ipopt_opts())
 
+try:
+    from Cython.Distutils import build_ext
+    cmdclass = {'build_ext': build_ext}
+except ImportError:
+    cmdclass = {}
+
 
 setup(
     name="yaipopt",
     version='0.1.dev2',
     test_suite='nose.collector',
     tests_require=['nose>=1.0'],
-    install_requires=['cython', 'numpy', 'setuptools>=18.0.1'],
+    install_requires=['cython', 'numpy', 'setuptools'],
     packages=find_packages(),
+    cmdclass=cmdclass,
     ext_modules=[wrapper],
     
     # metadata for upload to PyPI
